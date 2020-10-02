8bc25fbebe8fd1a6183238cf57eaad29a7be4055
Vivdean <vivdenkoanna@gmail.com>
2020-10-02 09:53:11 +0300

    Блок Абонементы(tablet и mobile)

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var jsTriggers = document.querySelectorAll('.tabs__trigger');
-//  var jsContents = document.querySelectorAll('.tabs__membership');
 
 jsTriggers.forEach(function (trigger) {
   trigger.addEventListener('click', function () {
     var id = trigger.getAttribute('data-tab');
-    console.log(id);
     var content = document.querySelector('.membership[data-tab="' + id + '"]');
     var activeTrigger = document.querySelector('.tabs__trigger.active');
     var activeContent = document.querySelector('.membership.active');
